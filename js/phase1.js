acaf3bd267c141fc50a7206eec32fc6a1c0ddaef
Catt McHale <91563580+thealyx0ne@users.noreply.github.com>
2021-10-21 12:09:35 -0400

    Update phase1.js

--- js/phase1.js
+++ js/phase1.js
@@ -1,1 +1,3 @@
-//this will be a javascript file that implements the use of a footer for accessibility purposes
+//this will be a javascript file that implements the use of a footer for accessibility purposes.
+//this will also be used to process the form inout from the search bar on the main page, 
+//which will allow the website to function properly as a search engine
